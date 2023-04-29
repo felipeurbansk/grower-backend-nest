de4234bfd9b0251aa4c025ca9c99b289f421b64a
Felipe Urbanski <felipeurbansk@gmail.com>
2023-04-28 23:34:53 -0300

    feat: import GrowModule

--- src/AppModules.ts
+++ src/AppModules.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { GrowModule } from './core/modules/GrowModule';
+import { GrowModule } from './modules/grows';
 
 @Module({
   imports: [GrowModule],
