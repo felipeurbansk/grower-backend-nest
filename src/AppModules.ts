f8e5d52d25e895051e4c1eab0a8f26234828e6fc
Felipe Urbanski <felipeurbansk@gmail.com>
2023-05-10 22:53:20 -0300

    feat: create hardwares routes group

--- src/AppModules.ts
+++ src/AppModules.ts
@@ -10,19 +10,34 @@ import { UserModule } from './modules/users';
 
 import { AuthModule } from './auth';
 import { FarmingModule } from './modules/farmings';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
   imports: [
+    AuthModule,
+    UserModule,
     GrowModule,
     SeedModule,
     LightModule,
+    PlantModule,
+    FarmingModule,
     BaseComponentModule,
     TemperatureModule,
     HumidtyModule,
-    PlantModule,
-    UserModule,
-    FarmingModule,
-    AuthModule,
+    RouterModule.register([
+      {
+        path: 'hardwares',
+        module: BaseComponentModule,
+      },
+      {
+        path: 'hardwares',
+        module: HumidtyModule,
+      },
+      {
+        path: 'hardwares',
+        module: TemperatureModule,
+      },
+    ]),
   ],
 })
 export class AppModule {}
