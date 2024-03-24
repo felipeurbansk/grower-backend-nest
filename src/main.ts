bfa603edd83b48e5d641754e8752a47eb7ae53ef
Felipe Urbanski <felipeurbansk@gmail.com>
2024-03-24 00:48:40 -0300

    feat: create vscode debug config

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './AppModules';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
